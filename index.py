fa23fe48267949a8cc4e15db6dadf5ad4f346db1
Projckt <projckt@protonmail.com>
2023-12-22 16:30:36 +0530

    fixes

--- index.py
+++ index.py
@@ -26,11 +26,7 @@ con = sqlite3.connect("db.sqlite")
 cur = con.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS shares (sessionId, selectedCardIds)")
 
-@app.get("/test")
-async def python_test():
-    print(owl_file_path)
-    return "Audit4SG app api"
-
+# DEV - Share cards
 class Share(BaseModel):
     sessionId: str
     selectedCardIds: list
@@ -46,6 +42,7 @@ async def save_to_db(data: Share):
         con.commit()
     return {"success": True}
 
+# DEV - Get reading data
 class Read(BaseModel):
     sessionId: str
 @app.post("/reading-data")
@@ -53,7 +50,40 @@ async def get_reading_data(data: Read):
     res = cur.execute('''SELECT * FROM shares WHERE sessionId = ?''', (data.sessionId,))
     return { "success": True, "payload": graph_jsonld, "readingData": res.fetchall()}
 
+# DEV - Get ontology
 @app.get("/")
 async def root():
     session_id = str(uuid.uuid4())
     return { "success": True, "payload": graph_jsonld, "sessionId": session_id}
+
+# ----------------------------------------------------------------------------------
+
+# PROD - Get Ontology
+@app.get("/ontology")
+async def get_ontology():
+    session_id = str(uuid.uuid4())
+    return { "success": True, "ontologyData": graph_jsonld, "sessionId": session_id}
+
+# PROD - Save Cards
+class Share(BaseModel):
+    sessionId: str
+    selectedCardIds: list
+@app.post("/cards")
+async def save_cards(data: Share):
+    res = cur.execute('''SELECT * FROM shares WHERE sessionId = ?''', (data.sessionId,))
+    if len(res.fetchall()) > 0:
+        cur.execute('''UPDATE shares SET selectedCardIds = ? WHERE sessionId = ?''', (json.dumps(data.selectedCardIds), data.sessionId,))
+        con.commit()
+    else:
+        row = [data.sessionId, json.dumps(data.selectedCardIds)]
+        cur.execute('INSERT INTO shares VALUES (?, ?)', row)
+        con.commit()
+    return {"success": True}
+
+# PROD - Get Cards
+class Read(BaseModel):
+    sessionId: str
+@app.get("/cards")
+async def get_cards(data: Read):
+    res = cur.execute('''SELECT * FROM shares WHERE sessionId = ?''', (data.sessionId,))
+    return { "success": True, "payload": graph_jsonld, "readingData": res.fetchall()}
